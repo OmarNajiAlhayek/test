1ea95743019e5d50634c574aae0f71ca52712108
Omar Naji Alhayek <onajialhayek@gmail.com>
2025-07-12 13:39:36 +0300

    Update test.c

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 int main() {
     printf("line number 1");
     printf("line number 2 modified locally");
-    printf("line number 2 modified remotly");
+    printf("line number 2 modified REMOT999LY");
     printf("line number 3");
     printf("line that it's number is 4");
     return 0;
